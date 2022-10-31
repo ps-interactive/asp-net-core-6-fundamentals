190109211ccf0bc3353a11989962cf0885408926
Gill Cleeren <GillCleeren@users.noreply.github.com>
2022-10-31 21:00:39 +0100

    Initial c'ted

---
+++ 25-checkpoint-blazor-data-binding/BethanysPieShopHRM/Models/EmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.IO.Pipelines;
+
+namespace BethanysPieShopHRM.Models
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly BethanysPieShopHRMDbContext _context;
+
+        public EmployeeRepository(BethanysPieShopHRMDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return _context.Employees.Include(c => c.Category).ToList();
+        }
+
+        public Employee? GetEmployeeById(int employeeId)
+        {
+            return _context.Employees.FirstOrDefault(p => p.EmployeeId == employeeId);
+        }
+    }
+}
